a49c6ff027462768cea909c6d71bfe4adb45061c
unknown <madjahed228@gmail.com>
2020-01-31 23:59:49 +0300

    something

--- bot.py
+++ bot.py
@@ -9,8 +9,23 @@ def start_message(message):
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет, мой создатель')
+        bot.send_message(message.chat.id, 'Привет')
     elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Прощай, создатель')
+        bot.send_message(message.chat.id, 'Прощай')
+    else:
+        bot.send_message(message.chat.id, 'Извините , но я не понимаю вас')
+@bot.message_handler(commands=['help']
+def command_help(message):
+    bot.reply_to(message, "Тебе понадобилась помощь?")
 
-bot.polling()
+@bot.message_handler(regexp='((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)')
+def command_url(message):
+    bot.reply_to(message, "Я не могу распознать этот url")
+
+@bot.message_handler(func=lambda message: message.document.mime_type == 'text/plain', content_types=['document'])
+def command_handle_document(message):
+    bot.reply_to(message, "Я получил присланный документ")
+
+
+
+bot.polling(none_stop=True)
