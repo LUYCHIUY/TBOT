325fbd17855d38680d147eefc5281057b502134a
unknown <madjahed228@gmail.com>
2020-02-02 00:26:21 +0300

    f

--- bot.py
+++ bot.py
@@ -2,25 +2,35 @@ import telebot
 import config
 import schedule
 import time
+#import apiai
+#import json
 
 bot = telebot.TeleBot(config.token)
+bot.polling(none_stop=True)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Оу , мне написали')
-    bot.send_sticker(message.chat.id, 'CAACAgQAAxkBAAIf7V41tb65v4vrhoBt-7ce2pevbf-AAAJ_AANLae4QVPPmclI6esMYBA')
+    if message == 'start':
+        bot.send_message(message.chat.id, 'Оу , мне написали')
+        bot.send_sticker(message.chat.id, 'CAACAgQAAxkBAAIf7V41tb65v4vrhoBt-7ce2pevbf-AAAJ_AANLae4QVPPmclI6esMYBA')
 
+
+def command_help(message):
+    if message == 'help':
+        bot.send_message(message, "Тебе понадобилась HelP?")
+
+
+# def textMessage(bot):
+# request = apiai.ApiAI('a7639ef21aec462ca7195db2523e373c').text_request() # Токен API к Dialogflow
+# request.lang = 'ru' # На каком языке будет послан запрос
+# request.session_id = 'BatlabAIBot' # ID Сессии диалога (нужно, чтобы потом учить бота)
+# request.query = bot.message.text # Посылаем запрос к ИИ с сообщением от юзера
 
 @bot.message_handler(commands=["ping"])
 def on_ping(message):
     bot.reply_to(message, "Я до сих пор работаю")
     bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIf9F41t5PWT3f5h-pvVEwLz3x4LyA6AAJ7AQACFKL1E6IZp7zvnesXGAQ')
-
-
-@bot.message_handler(commands=['help'])
-def command_help(message):
-    bot.send_message(message, "Тебе понадобилась HelP?")
 
 
 @bot.message_handler(content_types=['text'])
@@ -41,14 +51,12 @@ def command_handle_document(message):
     bot.reply_to(message, "Я получил присланный Document")
 
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
-
-
 @bot.message_handler(content_types=['text'])
 def mor_msg(message):
-    mor_msg = []
+    mor_msg = ['С добрым утром!']
     bot.send_message(message.chat.id, mor_msg)
+    bot.send_sticker()
+
 
 schedule.every().day.at("7.00").do(mor_msg)
 while True:
