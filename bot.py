ce24249fec6377f5073b8c45f350cf765fbff236
unknown <madjahed228@gmail.com>
2020-02-15 18:19:33 +0300

    fff

--- bot.py
+++ bot.py
@@ -1,50 +1,36 @@
-import telebot
-import config
+import os
+
 from flask import Flask, request
 
-secret = 'ghfegc26327jdhs'
-url = 'https://luybimuy.pythonanywhere.com/' + secret
-bot = telebot.TeleBot(config.token, threaded=False)
-bot.remove_webhook()
-bot.set_webhook(url=url)
+import telebot
 
-app = Flask(__name__)
-@app.route('/'+secret, methods=['POST'])
-def webhook():
-    update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
-    bot.process_new_updates([update])
-    return 'ok', 200
+TOKEN = '765437824:AAHiyUz4N8zTuv5Z435i6HNRREiJ2wS1A7I'
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
+
 
 @bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Оу , мне написали')
-    bot.send_sticker(message.chat.id, 'CAACAgQAAxkBAAIf7V41tb65v4vrhoBt-7ce2pevbf-AAAJ_AANLae4QVPPmclI6esMYBA')
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 
-@bot.message_handler(commands=['help'])
-def command_help(message):
-    bot.send_message(message, "Тебе понадобилась HelP?")
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
 
-@bot.message_handler(commands=["ping"])
-def on_ping(message):
-    bot.reply_to(message, "Я до сих пор работаю")
-    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIf9F41t5PWT3f5h-pvVEwLz3x4LyA6AAJ7AQACFKL1E6IZp7zvnesXGAQ')
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
 
-@bot.message_handler(content_types=['text'])
-def send_hi(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет)')
-    elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Пока(')
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=' https://tbot1283.herokuapp.com/' + TOKEN)
+    return "!", 200
 
 
-@bot.message_handler(regexp='((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)')
-def command_url(message):
-    bot.reply_to(message, "Я не могу распознать этот url")
-
-
-@bot.message_handler(func=lambda message: message.document.mime_type == 'text/plain', content_types=['document'])
-def command_handle_document(message):
-    bot.reply_to(message, "Я получил присланный Document")
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
