4817c8f8af191f14292d0da24ea89f6834cbd16c
unknown <madjahed228@gmail.com>
2020-02-15 13:57:31 +0300

    fff

--- bot.py
+++ bot.py
@@ -1,8 +1,32 @@
 import telebot
 import config
+import os
+from flask import Flask, request
+import logging
 
 bot = telebot.TeleBot(config.token)
 
+# Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
+if "HEROKU" in list(os.environ.keys()):
+    logger = telebot.logger
+    telebot.logger.setLevel(logging.INFO)
+
+    server = Flask(__name__)
+    @server.route("/bot", methods=['POST'])
+    def getMessage():
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
+    @server.route("/")
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url="https://dashboard.heroku.com/apps/tbot1283") # этот url нужно заменить на url вашего Хероку приложения
+        return "?", 200
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+else:
+    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.
+    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
+    bot.remove_webhook()
+    bot.polling(none_stop=True)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -39,4 +63,3 @@ def command_handle_document(message):
     bot.reply_to(message, "Я получил присланный Document")
 
 
-bot.infinity_polling(True)
